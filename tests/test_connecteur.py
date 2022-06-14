372f57affcf606f6101e1e1676ea1889acc52750
Julie <julie.laniau@loire-atlantique.fr>
2022-06-14 09:12:42 +0200

    fonction de test du connecteurs

--- tests/test_connecteur.py
+++ tests/test_connecteur.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import mock
 import pytest
 import django_webtest
+import os
 
-from django.contrib.contenttypes.models import ContentType
-from passerelle_test.models import TestConnector
-from passerelle.base.models import ApiUser, AccessRight
+# from django.contrib.contenttypes.models import ContentType
+# from passerelle_test.models import TestConnector
+# from passerelle.base.models import ApiUser, AccessRight
+
+import prefill_pdf.utils as utils
+import prefill_pdf.models as models
+# from prefill_pdf.models import Prefill_PDF
 
 @pytest.fixture
 def app(request):
@@ -22,3 +28,18 @@ def connector(db):
             codename='can_access', apiuser=api,
             resource_type=obj_type, resource_pk=connector.pk)
     return connector
+
+def test_pdftk():
+    cmd = "%s" % (utils.PDFTK_PATH)
+    output = utils.run_command(cmd)
+    assert 'SYNOPSIS' in output
+
+def test_cerfa_existed():
+    cerfa_name = 'cerfa_10072-02.pdf'
+    cerfa_file = os.path.join(models.template_dir, cerfa_name)
+    assert os.path.isfile(cerfa_file)
+
+def test_xfdf_existed():
+    xfdf_name = 'data.xfdf'
+    xfdf_file = os.path.join(models.template_dir, xfdf_name)
+    assert os.path.isfile(xfdf_file)
